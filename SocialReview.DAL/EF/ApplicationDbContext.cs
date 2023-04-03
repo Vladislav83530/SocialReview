b3feef0ffc1d198eb4c43caa295de509ac5f2a0c
Vladyslav Petrov <vlad.ptrv83530@gmail.com>
2023-04-03 15:54:56 +0300

    feat-database-setup: Set up initial database configuration
    - Create DbContext class and configure entities
    - Update Program.cs to register DbContext
    - Add connection string to appsettings.json

---
+++ SocialReview.DAL/EF/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialReview.DAL.Entities;
+
+namespace SocialReview.DAL.EF
+{
+    /// <summary>
+    /// Application database context
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Establishment> Establishments { get; set; }
+        public DbSet<EstablishmentPhoto> EstablishmentPhotos { get; set; }
+        public DbSet<EaterieReview> EaterieReviews { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        { 
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>()
+                .HasMany<EaterieReview>()
+                .WithOne(e => e.User)
+                .HasForeignKey(k => k.UserId);
+
+            builder.Entity<Establishment>()
+                .HasMany<EaterieReview>()
+                .WithOne(e => e.Establishment)
+                .HasForeignKey(c => c.EstablishmentId);
+
+            builder.Entity<Establishment>()
+                .HasMany<EstablishmentPhoto>()
+                .WithOne(e => e.Establishment)
+                .HasForeignKey(c => c.EstablishmentId);
+        }
+    }
+}
